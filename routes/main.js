94caaf3019796c468e317462ca27b232b198b920
RuqiyaG <ruqiyageelle@gmail.com>
2025-10-23 00:48:01 +0100

    added some html changes

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/",(req,res) =>res.send("Hello World"));
 router.get("/about", (req,res) => res.send("<h1>This is the about page</h1>"));
-router.get("/contact", (req,res) => res.send("<h1>This is the contact page</h1>  Email: RG123@gmail.com    Mobile: 9876542321"));
+router.get("/contact", (req,res) => res.send("<h1>This is the contact page</h1>  <p> <strong>Email:</strong> RG123@gmail.com<br><br>    <strong>Mobile:</strong> 9876542321</p>"));
 router.get("/date", (req,res) => res.send("<h1>Today's Date is...</h1>"))
 
 module.exports = router;
