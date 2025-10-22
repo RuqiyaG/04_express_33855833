d1603bc7c52115aef4648956122056a9d7f8140f
RuqiyaG <ruqiyageelle@gmail.com>
2025-10-23 00:43:27 +0100

    bolder date

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.get("/",(req,res) =>res.send("Hello World"));
 router.get("/about", (req,res) => res.send("<h1>This is the about page</h1>"));
 router.get("/contact", (req,res) => res.send("<h1>This is the contact page</h1>  Email: RG123@gmail.com    Mobile: 9876542321"));
-router.get("/date", (req,res) => res.send("Today's Date is..."))
+router.get("/date", (req,res) => res.send("<h1>Today's Date is...</h1>"))
 
 module.exports = router;
